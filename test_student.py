00e11b6a2e70cf0d23ba14bc302a71663c3e86bf
peterudu <puduevborho7@gmail.com>
2023-02-12 14:30:35 +0000

    Using TDD, create code

--- test_student.py
+++ test_student.py
@@ -5,7 +5,13 @@ from student import Student
 class TestStudent(unittest.TestCase):
     def test_full_name(self):
         student = Student("John", "Doe")
+
         self.assertEqual(student.full_name, "John Doe")
+
+    def test_email(self):
+        student = Student("John", "Doe")
+
+        self.assertEqual(student.email, "john.doe@email.com")
 
     def test_alert_santa(self):
         student = Student("John",  "Doe")
